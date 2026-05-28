c43b26f1b61c664b8ef8c1539b704c44689fd5a3
Rajottam Pradhan <rajottampradhan.codehimalaya@gmail.com>
2026-05-28 23:56:34 +0545

    Initial commit

---
+++ src/main/java/io/vl/voltledger/VoltLedgerApplication.java
@@ -0,0 +1,13 @@
+package io.vl.voltledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoltLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoltLedgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/vl/voltledger/VoltLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.vl.voltledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoltLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
