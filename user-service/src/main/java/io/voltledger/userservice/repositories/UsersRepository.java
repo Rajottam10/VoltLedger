7a50cdeca9c0705f75e471ce97027f54d5373d10
Rajottam Pradhan <rajottampradhan.codehimalaya@gmail.com>
2026-06-03 23:23:23 +0545

    fix: added users repository

---
+++ user-service/src/main/java/io/voltledger/userservice/repositories/UsersRepository.java
@@ -0,0 +1,9 @@
+package io.voltledger.userservice.repositories;
+
+import io.voltledger.userservice.entities.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users, Long> {
+}
