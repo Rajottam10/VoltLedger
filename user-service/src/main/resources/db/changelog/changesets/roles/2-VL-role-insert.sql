2380de67d417b79af568c70b2a34bf83c87329f8
Rajottam Pradhan <rajottampradhan@gmail.com>
2026-07-07 17:53:06 +0545

    fix: updated pom in parent and in config-server & added liquibase

--- user-service/src/main/resources/db/changelog/changesets/roles/2-VL-role-insert.sql
+++ user-service/src/main/resources/db/changelog/changesets/roles/2-VL-role-insert.sql
@@ -1,6 +1,8 @@
 --liquibase formatted sql
 
 --changeset rajottam.pradhan: VL-002
+--preconditions onFail:CONTINUE onError:HALT
+--precondition-sql-check expectedResult:0 SELECT COUNT(*) FROM roles
 INSERT INTO roles (name, description) VALUES
                                           ('SUPER_ADMIN', 'Full platform access'),
                                           ('LANDLORD',    'Manages properties and tenants'),
