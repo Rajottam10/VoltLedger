9ee04cb40c14a9a8f0d5c96545ff8aff39c429c6
Rajottam Pradhan <rajottampradhan@gmail.com>
2026-08-05 14:29:10 +0545

    fix: updated pom and liquibase files

--- user-service/src/main/resources/db/changelog/changesets/roles/2-VL-roles-insert.sql
+++ user-service/src/main/resources/db/changelog/changesets/roles/2-VL-roles-insert.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset rajottam.pradhan: VL-002
+--changeset rajottam.pradhan:VL-roles-002
 --preconditions onFail:CONTINUE onError:HALT
 --precondition-sql-check expectedResult:0 SELECT COUNT(*) FROM roles
 INSERT INTO roles (name, description) VALUES
